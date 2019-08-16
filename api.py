3b8096cc9e4bc571a922e93e29c701a964db1d7c
prtk94 <z5175940@student.unsw.edu.au>
2019-08-16 16:57:38 +1000

    basic error handling and prod fix

--- api.py
+++ api.py
@@ -99,8 +99,8 @@ if __name__ == '__main__':
     # run the application
 
     port = int(os.environ.get('PORT', 33507)) 
-    #app.run(host='0.0.0.0', port=port)
-    app.run(debug=True, port='7000')
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True, port='7000')
 
 '''
 #output model. not coming up on schema yet
